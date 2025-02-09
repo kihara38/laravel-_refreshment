fa0aa2d0df088f75ba6b991f66555e41e1019eea
kihara38 <85784773+kihara38@users.noreply.github.com>
2025-02-09 10:39:28 +0300

    creating form

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ninja;
+use App\Models\Dojo;
 use Illuminate\Http\Request;
 
 class NinjaController extends Controller
@@ -23,7 +24,24 @@ class NinjaController extends Controller
 
     public function create()
     {
-        return view('ninjas.create');
+        $dojos = Dojo::all();
+
+        return view('ninjas.create', ['dojos' => $dojos]);
     }
-    public function store() {}
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'skill' => 'required|integer|min:0|max:100',
+            'bio' => 'required|string|min:20|max:1000',
+            'dojo_id' => 'required|exists:dojos,id',
+        ]);
+
+        Ninja::create($validated);
+
+        return redirect()->route('ninjas.index');
+    }
+
+    public function destoy($id) {}
 }
